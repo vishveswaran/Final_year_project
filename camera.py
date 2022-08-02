dd3aa2deb522e45915f726c704e12cd4105315a0
vishpi <vishveswaran004@gmail.com>
2022-08-02 13:41:00 +0530

    updated the camera program

--- camera.py
+++ camera.py
@@ -1,3 +1,15 @@
 import cv2
-print("hello")
-print('JOJO')
+import numpy
+
+cap = cv2.VideoCapture(0)
+cap.set(10, 50)
+cap.set(3,640)
+cap.set(4,480)
+
+while True:
+    success,img = cap.read()
+    cv2.imshow('frame', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
